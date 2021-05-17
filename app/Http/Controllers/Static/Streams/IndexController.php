1335e2945e4d32f5e53306c967256564f6c4969e
Steve McDougall <juststevemcd@gmail.com>
2021-05-17 20:01:29 +0100

    routing from controller to view

--- app/Http/Controllers/Static/Streams/IndexController.php
+++ app/Http/Controllers/Static/Streams/IndexController.php
@@ -1,20 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Static\Streams;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 
 class IndexController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View
     {
-        //
+        return view('static.streams.index');
     }
 }
